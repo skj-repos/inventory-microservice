b096235eb7de7f66e834ba363158100fa6e9c924
pradeep <pradeep.kumar44@gmail.com>
2022-11-16 14:13:38 +0530

    committing the changes to inventory microservice

--- src/main/java/com/classpath/inventory/InventoryMicroserviceApplication.java
+++ src/main/java/com/classpath/inventory/InventoryMicroserviceApplication.java
@@ -2,11 +2,8 @@ package com.classpath.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
 
 public class InventoryMicroserviceApplication {
 
