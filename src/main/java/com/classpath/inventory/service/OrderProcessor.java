280a5aacc86bcb743e161ec86920bd0e36890734
pradeep <pradeep.kumar44@gmail.com>
2023-02-17 10:03:06 +0530

    committing the changes

--- src/main/java/com/classpath/inventory/service/OrderProcessor.java
+++ src/main/java/com/classpath/inventory/service/OrderProcessor.java
@@ -1,20 +1,14 @@
 package com.classpath.inventory.service;
 
-import java.util.function.Consumer;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-
-import com.classpath.inventory.event.OrderEvent;
 
 @Component
 public class OrderProcessor {
 	
-	@Bean
-	public Consumer<Message<OrderEvent>> consumer() {
-	    return message -> System.out.println("process the order " + message);
-	}
+	/*
+	 * @Bean public Consumer<Message<OrderEvent>> consumer() { return message ->
+	 * System.out.println("process the order " + message); }
+	 */
 
 	
 
